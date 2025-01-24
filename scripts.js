f4fdf460f236c0af9a28158789618a0b4e92af9e
NeferMR <ricaurten@uninorte.edu.co>
2025-01-24 10:06:50 -0500

    First Commit

---
+++ scripts.js
@@ -0,0 +1,111 @@
+const texts = [
+  "Ingeniero de Sistemas",
+  "Entusiasta de la programación",
+  "Desarrollador FullStack",
+  "Desarrollador de automatización",
+  "Apasionado por la tecnología",
+  "Desarrollador de Software",
+  "Programador de Machine Learning",
+];
+let currentText = 0;
+let currentChar = 0;
+let isDeleting = false;
+let delay = false;
+
+const typedTextElement = document.getElementById("typed-text");
+
+function type() {
+  const currentString = texts[currentText];
+  if (isDeleting) {
+    typedTextElement.innerHTML = currentString.substring(0, currentChar - 1);
+    currentChar--;
+    if (currentChar === 0) {
+      isDeleting = false;
+      currentText = (currentText + 1) % texts.length;
+    }
+    setTimeout(type, 100);
+  } else {
+    typedTextElement.innerHTML = currentString.substring(0, currentChar + 1);
+    currentChar++;
+    if (currentChar === currentString.length) {
+      if (!delay) {
+        delay = true;
+        setTimeout(() => {
+          isDeleting = true;
+          delay = false;
+          type();
+        }, 3000);
+        return;
+      }
+    }
+    setTimeout(type, 200);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+});
+
+let oldSection = "inicio";
+let newSection = "inicio";
+
+document.addEventListener("scroll", () => {
+  let sections = document.querySelectorAll("section");
+  const viewportHeight = window.innerHeight;
+  let maxVisibleSection = null;
+  let maxVisiblePercentage = 0;
+
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    const visibleHeight =
+      Math.min(rect.bottom, viewportHeight) - Math.max(rect.top, 0);
+    const visiblePercentage = (visibleHeight / rect.height) * 100;
+
+    if (visiblePercentage > maxVisiblePercentage) {
+      maxVisiblePercentage = visiblePercentage;
+      maxVisibleSection = section;
+    }
+  });
+
+  sections.forEach((section) => {
+    if (section.classList === maxVisibleSection.classList) {
+      newSection = section.className;
+      console.log(newSection, oldSection);
+      if (newSection === oldSection) return;
+      console.log(
+        document.querySelector(`#${newSection}`),
+        document.querySelector(`#${oldSection}`)
+      );
+      document.querySelector(`#${newSection}`).classList.add("current");
+      document.querySelector(`#${oldSection}`).classList.add("notCurrent");
+      document.querySelector(`#${oldSection}`).classList.remove("current");
+      document.querySelector(`#${newSection}`).classList.remove("notCurrent");
+      oldSection = newSection;
+    }
+  });
+});
+
+function mostrarventana(titulo, texto, imagenes, link) {
+  document.querySelector(".ventana").style.visibility = "visible";
+  document.querySelector(".ventana").innerHTML = `
+  <div class="ventana-contenido">
+    <h3>${titulo}</h3>
+    <img src="src/x.png" alt="" class="x" onclick="ocultarventana()">
+    <div>
+      <span>
+          ${texto}
+          <br>
+          <br>
+          ${link ? `<a href="${link}" class="link">Link al repositorio</a>` : ""}
+        </span>
+        <div class="fotos">
+          ${imagenes.map((imagen) => `<img src="${imagen}" alt="" style="margin: auto" />`).join("")}
+        </div>
+    </div>
+  </div>
+  `;
+}
+
+function ocultarventana() {
+  document.querySelector(".ventana").style.visibility = "hidden";
+}
